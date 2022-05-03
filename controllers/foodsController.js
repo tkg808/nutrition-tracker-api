66265a9bdd2d584abf36fc1f2ceac6c75ca6ff43
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-05-03 16:43:39 -0700

    Add CRD functionality for Food.

--- controllers/foodsController.js
+++ controllers/foodsController.js
@@ -12,4 +12,28 @@ router.get("/", (request, response) =>
     .catch(console.error);
 });
 
+// SHOW -- get by id.
+router.get("/:id", (request, response) =>
+{
+  Food.findById(request.params.id)
+    .then((food) => response.json(food))
+    .catch(console.error);
+});
+
+// CREATE -- add new.
+router.post("/", (request, response) =>
+{
+  Food.create(request.body)
+    .then((newFood) => response.json(newFood))
+    .catch(console.error);
+});
+
+// DELETE -- remove by id.
+router.delete("/:id", (request, response) =>
+{
+  Food.findByIdAndDelete(request.params.id)
+    .then((food) => response.json(food))
+    .catch(console.error);
+});
+
 module.exports = router;
