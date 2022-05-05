a33c6bcf00003d224457f878d7d2e35b806f27b1
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-05-05 13:29:05 -0700

    Update seed with Users.

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,20 @@
+const User = require("../models/User");
 const Food = require("../models/Food");
-const foodData = require("./foodData.json");
+const userData = require("./dataUsers.json");
+const foodData = require("./dataFoods.json");
 
-Food.deleteMany({})
-  .then(() => Food.insertMany(foodData))
+User.deleteMany({})
+  .then(() => Food.deleteMany({}))
+  .then(() => User.insertMany(userData))
+  .then((users) => 
+  {
+    // Sets owner for all foods before seeding.
+    foodData.forEach((food) => food.owner = users[0]._id);
+
+    return foodData;
+  })
+  .then((foods) => Food.insertMany(foods))
+  .then((foods) => console.log(foods))
   .then(console.log("Success!"))
   .catch(console.error)
   .finally(process.exit);
