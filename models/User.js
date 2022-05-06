e49d622287da25c79ed482efc6a27b463488da9a
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-05-06 15:10:25 -0700

    Update User model.

--- models/User.js
+++ models/User.js
@@ -2,6 +2,12 @@ const mongoose = require("../db/connection");
 
 const UserSchema = new mongoose.Schema(
   {
+    username:
+    {
+      type: String,
+      required: true,
+      unique: true
+    },
     email:
     {
       type: String,
