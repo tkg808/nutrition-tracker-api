e65a065b08a56ff955a19e30d9ea6175c7b1682d
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-05-09 14:39:04 -0700

    Update seed file to hash passwords.

--- db/seed.js
+++ db/seed.js
@@ -3,6 +3,14 @@ const Food = require("../models/Food");
 const userData = require("./dataUsers.json");
 const foodData = require("./dataFoods.json");
 
+// Handles hashing.
+const bcrypt = require("bcrypt");
+
+let hashed = "";
+
+Promise.resolve(bcrypt.hash(userData[0].password, 10))
+  .then((hash) => hashed += hash);
+
 User.deleteMany({})
   .then(() => Food.deleteMany({}))
   .then(() => User.insertMany(userData))
@@ -15,6 +23,7 @@ User.deleteMany({})
   })
   .then((foods) => Food.insertMany(foods))
   .then((foods) => console.log(foods))
+  .then(() => User.updateMany({}, { "$set": { "password": hashed } }))
   .then(console.log("Success!"))
   .catch(console.error)
   .finally(process.exit);
